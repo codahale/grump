d10f3e01d6d2ccc2d2d249f0594acff54dedbca8
Coda Hale <coda.hale@gmail.com>
2014-09-13 21:40:05 -0700

    Move framed reader/writer to its own file.

--- grump.go
+++ grump.go
@@ -3,9 +3,7 @@ package grump
 import (
 	"crypto/rand"
 	"crypto/sha512"
-	"encoding/binary"
 	"errors"
-	"hash"
 	"io"
 	"io/ioutil"
 	"math/big"
@@ -512,76 +510,4 @@ func secureShuffle(keys [][]byte) ([][]byte, error) {
 	}
 
 	return k, nil
-}
-
-// framedReader reads protobuf messages, framed with a 32-bit little-endian
-// length prefix.
-type framedReader struct {
-	r            io.Reader
-	sizeBuf, buf []byte
-	h            hash.Hash
-}
-
-// digest returns the SHA-512 digest of all the data written.
-func (fr framedReader) digest() []byte {
-	return fr.h.Sum(nil)
-}
-
-// readMessage reads the next message in the reader into the given protobuf.
-func (fr framedReader) readMessage(msg proto.Message) error {
-	if _, err := io.ReadFull(fr.r, fr.sizeBuf); err != nil {
-		return err
-	}
-
-	size := int(binary.LittleEndian.Uint32(fr.sizeBuf))
-	if size > MaxFrameSize {
-		return ErrFrameTooLarge
-	}
-
-	if len(fr.buf) < size {
-		fr.buf = make([]byte, size)
-	}
-
-	n, err := io.ReadFull(fr.r, fr.buf)
-	if err != nil {
-		return err
-	}
-
-	_, _ = fr.h.Write(fr.sizeBuf)
-	_, _ = fr.h.Write(fr.buf[:n])
-
-	return proto.Unmarshal(fr.buf[:n], msg)
-}
-
-// framedWriter writes protobuf messages, framed with a 32-bit little-endian
-// length prefix.
-type framedWriter struct {
-	w       io.Writer
-	sizeBuf []byte
-	h       hash.Hash
-}
-
-// digest returns the SHA-512 digest of all the data written.
-func (fw framedWriter) digest() []byte {
-	return fw.h.Sum(nil)
-}
-
-// writeMessage writes the given protobuf message into the writer.
-func (fw framedWriter) writeMessage(msg proto.Message) error {
-	buf, err := proto.Marshal(msg)
-	if err != nil {
-		return err
-	}
-
-	binary.LittleEndian.PutUint32(fw.sizeBuf, uint32(len(buf)))
-
-	if _, err := fw.w.Write(fw.sizeBuf); err != nil {
-		return err
-	}
-
-	_, _ = fw.h.Write(fw.sizeBuf)
-	_, _ = fw.h.Write(buf)
-
-	_, err = fw.w.Write(buf)
-	return err
 }
